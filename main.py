c76f63f11472d60f4d73606d64113d8dca14744f
ellman121 <ellman121@gmail.com>
2019-09-12 16:28:34 +0200

    Adds file info printing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Intertag
 # Interactive metadata enditor for audio files
 
-import os
+import pprint
 import sys
 import mutagen
 
@@ -14,6 +14,7 @@ TRACK_CNT_KEY = 'TXXX'
 YEAR_KEY = 'TYER'
 METADATA_KEYS = [ARTIST_KEY, ALBUM_ARTIST_KEY, ALBUM_KEY, TITLE_KEY, TRACK_NUM_KEY, TRACK_CNT_KEY,YEAR_KEY]
 
+# Yeah, it's a global.  Sue me, it's a 100 line python script to help manage metadata for mp3 tiles
 cache = {
     ARTIST_KEY: set([]),
     ALBUM_ARTIST_KEY: set([]),
@@ -28,36 +29,60 @@ validExtensions = ['mp3']
 
 
 def checkFiles(filelist):
+    ret = []
     for f in filelist:
-        ext = f.split('.')[-1]
-        if not os.path.isfile(f) or ext not in validExtensions:
-            print("Invalid file: %s" % (f))
-            return False
-    return True
+        try:
+            with open(f, 'rb') as fle:
+                f = mutagen.File(fle)
+                ret.append(f)
+        except mutagen.MutagenError as e:
+            print("File unacceptable", e)
+            return ([], False)
+    return (ret, True)
 
 
-def readFile(f):
-    try:
-        with open(f, 'rb') as fle:
-            f = mutagen.File(fle)
-    except mutagen.MutagenError as e:
-        print("File unacceptable", e)
-        return
+def valueForKey(key, mutagenFile):
+    if key in mutagenFile:
+        return mutagenFile[key].text[0]
+    
+    return ''
 
-    print(f)
 
+def addExistingMetadataToCache(f):
     for k in METADATA_KEYS:
         if k in f:
-            cache[k].add(f[k].text[0])
+            cache[k].add(valueForKey(k, f))
+
+
+def printFileInfo(filename, mutagenFile):
+    # Print the file info to the terminal
+    print("** FILE: " + filename + "\n")
+    print("Artist Name   : " + valueForKey(ARTIST_KEY, mutagenFile))
+    print("Album Artist  : " + valueForKey(ALBUM_ARTIST_KEY, mutagenFile))
+    print("Album Name    : " + valueForKey(ALBUM_KEY, mutagenFile))
+    print("Year Released : " + valueForKey(YEAR_KEY, mutagenFile))
+    print("Track Title   : " + valueForKey(TITLE_KEY, mutagenFile))
+    print("Track Number  : " + valueForKey(TRACK_NUM_KEY, mutagenFile))
+    print("Total Tracks  : " + valueForKey(TRACK_CNT_KEY, mutagenFile))
+
+
+def completeMetadata(filename, mutagenFile):
+    printFileInfo(filename, mutagenFile)
 
 
 if __name__ == "__main__":
     files = sys.argv[1:]
 
-    # Check command line argumetns
-    if not checkFiles(files):
+    # Check files on the way in
+    mutagenFiles, ok = checkFiles(files)
+    if not ok:
         print("Error checking files")
         sys.exit()
 
-    for f in files:
-        readFile(f)
+    print("Reading in files, building metadta cache")
+    for f in mutagenFiles:
+        addExistingMetadataToCache(f)
+    print("Existing metadata added to cache")
+
+    for fname, mutafile in zip(files, mutagenFiles):
+        completeMetadata(fname, mutafile)
